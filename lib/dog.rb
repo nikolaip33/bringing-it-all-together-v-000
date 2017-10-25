34457ded8b4fb19ee9722a912881fd93861ef175
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-25 10:48:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,4 +63,8 @@ class Dog
     self.find_or_create_by(name:row[1], breed:row[2])
   end
 
+  def self.find_by_name(name)
+    result = DB[:conn].execute("SELECT * FROM dogs WHERE name = ?", name)
+  end
+
 end #class Dog
